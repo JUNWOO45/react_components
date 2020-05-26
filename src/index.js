f167ac9d3b770f35d84b607ee0634242d79b5056
junwoo45 <sinyoorang@gmail.com>
2020-05-26 17:35:14 +0900

    feat: semantic ui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src="" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Junwoo
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 6:00PM
+                        </span>
+                    </div>
+                    <div className="text">Hello World!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

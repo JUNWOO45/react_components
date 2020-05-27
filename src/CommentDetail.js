a806da325b97b314c83abebefba18648d441ef94
junwoo45 <sinyoorang@gmail.com>
2020-05-27 14:19:42 +0900

    feat: 여러개의 props전달

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+// import ReactDOM from 'react-dom';
+// import faker from 'faker';
 
 const CommentDetail = (props) => {
     console.log('props : ', props);
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                {/* <img alt="avatar" src={faker.image.avatar()} /> */}
+                <img alt="avatar" src={props.avatar} />
             </a>
             <div className="content">
                 <a href="/" className="author">
@@ -15,10 +16,11 @@ const CommentDetail = (props) => {
                 </a>
                 <div className="metadata">
                     <span className="date">
-                        Today at 6:00PM
+                        {/* Today at 6:00PM */}
+                        {props.timeAgo}
                     </span>
                 </div>
-                <div className="text">Hello World!</div>
+                <div className="text">{props.comment}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Junwoo"/>
-            <CommentDetail author="Gaon"/>
+            <CommentDetail author="Sam" timeAgo="Today at 4:00PM" comment="Hello World! - 1" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Junwoo" timeAgo="Yesterday at 5:00PM"comment="Hello World! - 2"avatar={faker.image.avatar()} />
+            <CommentDetail author="Gaon" timeAgo="Today at 12:00PM" comment="Hello World! - 3" avatar={faker.image.avatar()}/>
         </div>
     );
 };
